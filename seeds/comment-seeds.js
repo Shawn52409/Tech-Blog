bd896543ffae8c862bb51e81cdcda058821febce
Shawn Miller <shawnsmm@gmail.com>
2021-11-01 15:09:24 -0400

    seed data

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,17 @@
+const Comment = require('../models');
+
+const commentData = [
+    {
+        user_id: 1,
+        post_id: 4,
+        comment_text: "It lets work with only the keys of an object"
+    },
+    {
+        user_id: 2,
+        post_id: 4,
+        comment_text: "I believe React seems to be the sought after skill"
+    }
+]
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
--- seeds/posting-seeds.js
+++ seeds/posting-seeds.js
@@ -5,8 +5,28 @@ const postingData = [
         post_title: "handlebars!",
         post_content: "I really need some help on handlebars.  Anyone recommend any good sites that can help me understand it better?",
         user_id: 1
-    }
+    },
+    {
+        post_title: "Web languages",
+        post_content: "What is the computer language that most employers are looking for these days?",
+        user_id: 2
+    },
+    {
+        post_title: "React",
+        post_content: "What is the best book to help me learn React?",
+        user_id: 3
+    },
+    {
+        post_title: "...",
+        post_content: "What does ... mean in JS?",
+        user_id: 4
+    },
+    {
+        post_title: "Front end style question",
+        post_content: "What is a jass.css file refer too?",
+        user_id: 5
+    },
 ]
-const seedPosts = () => Posting.bulkCreate(postingData);
+const seedPostings = () => Posting.bulkCreate(postingData);
 
-module.exports = seedPosts;
+module.exports = seedPostings;
