d8c90e60d1ecef3db4af9841f7db448a485edb5a
Shawn Miller <shawnsmm@gmail.com>
2021-11-03 22:52:27 -0400

    wip, troubleshooting

--- public/js/login.js
+++ public/js/login.js
@@ -2,20 +2,22 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const name = document.querySelector('#name-login').value.trim();
+  const username = document.querySelector('#name-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (name && password) {
+  if (username && password) {
     // Send a POST request to the API endpoint
+    console.log(username);
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({name, password }),
+      body: JSON.stringify({username, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/');
+      alert('You are now logged in.')
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
@@ -25,28 +27,27 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
+  const username = document.querySelector('#name-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+  console.log(username);
+  console.log(password);
 
-  if (name && password) {
+  if (username && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, password }),
+      body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
     });
-
+    alert('Im here')
     if (response.ok) {
-      document.location.replace('/');
+      alert('You are now signed up and logged in.');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
   }
 };
 
-document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
